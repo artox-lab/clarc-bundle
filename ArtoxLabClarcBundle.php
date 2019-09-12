d46a1f8dc7f6f0559786c398a5dad09f5b3359b9
Artur Turchin <a.turchin@artox.com>
2019-09-12 10:49:33 +0300

    docs(comments): replace todo blocks with comments
    US-30728

--- ArtoxLabClarcBundle.php
+++ ArtoxLabClarcBundle.php
@@ -13,5 +13,5 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class ArtoxLabClarcBundle extends Bundle
 {
-
+    const CONFIG_BUNDLE_NAMESPACE = 'artox_lab_clarc';
 }
--- Core/UseCases/Interfaces/PaginatorInterface.php
+++ Core/UseCases/Interfaces/PaginatorInterface.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * todo: comment
+ * Paginator interface
  *
  * @author Artur Turchin <a.turchin@artox.com>
  */
