2ed1c5151f8866753bdfc39af7d5a1ead18fe65d
i.rakovets <i.rakovets@artox.com>
2021-07-09 10:39:04 +0300

    feat(api): add cached response method

--- Core/Interfaces/UI/API/Controllers/AbstractApiController.php
+++ Core/Interfaces/UI/API/Controllers/AbstractApiController.php
@@ -64,6 +64,19 @@ class AbstractApiController extends AbstractController
         $this->commandBus        = $commandBus;
         $this->queryBus          = $queryBus;
     }
+    
+    /**
+     * Response with Cache-Control header
+     *
+     * @param Response $response Response
+     * @param int      $lifetime Cache max age
+     *
+     * @return Response
+     */
+    public function cached(Response $response, int $lifetime) : Response
+    {
+        return $response->setMaxAge($lifetime);
+    }
 
     /**
      * Response of creating resource
